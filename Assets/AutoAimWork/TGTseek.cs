8f970d299dbfde2b103a020f756abaa36b4f2e74
Mehmet Emin Aksoy <mehmet.emin.aksoy@outlook.com>
2020-05-15 13:36:21 +0300

    Added Guided Missiles
    Press P to fire

---
+++ Assets/AutoAimWork/TGTseek.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TGTseek : MonoBehaviour
+{
+    public GameObject MSSL;
+    public float MSSLspeed = 1f;
+    public Rigidbody2D rb2d;
+
+    Vector3 MSSLhead;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        RotateMSSL();
+        PropelMSSL();
+        MSSLonTGTbail();
+    }
+    void RotateMSSL()
+    {
+        transform.right = GameObject.FindGameObjectWithTag("TGT").transform.position - MSSL.transform.position;
+    }
+    void PropelMSSL()
+    {
+        MSSLhead = GameObject.FindGameObjectWithTag("TGT").transform.position - MSSL.transform.position;
+        rb2d.AddForce(MSSLhead*MSSLspeed, ForceMode2D.Force);
+    }
+    void MSSLonTGTbail()
+    {
+        if(GameObject.FindGameObjectWithTag("TGT").transform.position == MSSL.transform.position)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/AutoAimWork/autoaim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoaim : MonoBehaviour
+{
+    public GameObject Projectile;
+    public GameObject Thrower;
+
+    private Vector3 LaunchPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            Spawn();
+        }
+    }
+    void Spawn()
+    {
+        LaunchPosition = Thrower.transform.position;
+        Instantiate(Projectile,LaunchPosition, Quaternion.identity);
+    }
+}
